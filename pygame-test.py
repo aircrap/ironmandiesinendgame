bcab1d4a17b56b993971a06255e46b359165e70d
Garrett Hale <gthkeno@gmail.com>
2020-05-10 17:37:55 -0500

    + added frame limiter

--- pygame-test.py
+++ pygame-test.py
@@ -1,5 +1,8 @@
 import pygame
 
+def clamp (val, low, high):
+    return max (low, min (val, high))
+
 # Simple Controls handler using boolean states
 class Controller:
     holds = {
@@ -12,13 +15,13 @@ class Controller:
     def check_events (self, key, eventUp):
         self.holds [key] = eventUp == pygame.KEYDOWN
 
-    def get_hold (self, key):
+    def get_hold (self, key) -> bool:
         return self.holds [key]
 
-    def get_direction_x (self):
+    def get_direction_x (self) -> int:
         return self.holds [pygame.K_d] - self.holds [pygame.K_a]
 
-    def get_direction_y (self):
+    def get_direction_y (self) -> int:
         return self.holds [pygame.K_s] - self.holds [pygame.K_w]
 
 # declare our controller
@@ -26,25 +29,32 @@ gamecon = Controller()
 
 # basic pygame window setup, keep screen variable for rendering
 pygame.init()
-screen = pygame.display.set_mode([800, 600])
+screen = pygame.display.set_mode([1280, 720])
 pygame.display.set_caption ("ubuntu lmao")
 
-x = 0
-y = 0
+x = 0.0
+y = 0.0
 
+# clock keeps the framerate at 60FPS while making sure the game runs
+# at a constant speed
+clock = pygame.time.Clock()
 running = True
 while running:
+    deltaTime = clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+        elif event.type == pygame.KEYDOWN:
+            gamecon.check_events (event.key, event.type)
+        elif event.type == pygame.KEYUP:
             gamecon.check_events (event.key, event.type)
 
-    x += gamecon.get_direction_x()
-    y += gamecon.get_direction_y()
+    x += gamecon.get_direction_x() * deltaTime
+    y += gamecon.get_direction_y() * deltaTime
 
     screen.fill((255, 255, 255))
-    pygame.draw.circle(screen, (0, 0, 255), (x, y), 75)
+    pygame.draw.circle(screen, (0, 0, 255), (int (x), int (y)), 75)
     pygame.display.flip()
 
 pygame.quit()
